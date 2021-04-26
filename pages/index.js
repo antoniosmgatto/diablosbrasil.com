ca39a78cff705c73c7fcbde909b8cc67eb3b7f16
Antonio Gatto <antonio.s.m.gatto@gmail.com>
2021-04-26 00:13:41 -0300

    adding html metatags

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,37 @@ export default function Home() {
     <div>
       <Head>
         <title>Diablos MC Brasil</title>
+        <meta
+          name="description"
+          content="DIABLOS MC - Associação de motociclistas que buscam os verdadeiros ideais de liberdade e expressão, pelas longas estradas desse inferno."
+        />
+        <meta name="keywords" content="Diablos, Diablos MC, Motociclismo" />
+        <meta name="author" content="Antonio Gatto" />
 
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main>
-        <h1>
-          Diablos MC Brasil
-        </h1>
+        <h1>Diablos MC Brasil</h1>
       </main>
     </div>
-  )
+  );
 }
