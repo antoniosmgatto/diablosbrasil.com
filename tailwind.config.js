832352c67bbae28b635e1f2ef245a92da8af879b
Antonio Gatto <antonio.s.m.gatto@gmail.com>
2022-06-15 10:30:31 -0300

    upgrading dependencies and settings to the latest versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       zIndex: {
-        '-10': '-10',
-      }
+        "-10": "-10",
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
