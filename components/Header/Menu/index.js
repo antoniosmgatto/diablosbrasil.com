e88a798c345e58dc832f4180fbb3f276dbd9987f
Antonio Gatto <antonio.s.m.gatto@gmail.com>
2022-06-28 15:30:34 -0300

    fix Hydration failed because the initial UI does not match what was rendered on the server

--- components/Header/Menu/index.js
+++ components/Header/Menu/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Mobile from "./Mobile";
 import Desktop from "./Desktop";
 import { useMediaQuery } from "react-responsive";
@@ -10,12 +10,15 @@ const Menu = () => {
      { path: "#contact", label: "Contato" },
    ];
 
+   const [mounted, setMounted] = useState(false);
    const isBigScreen = useMediaQuery({ query: '(min-width: 1280px)'})
 
+   useEffect(() => { setMounted(true)}, [])
+
   return (
-    <div>
-      { isBigScreen ? <Desktop links={links} /> : <Mobile links={links} /> }
-    </div>
+    <>
+      { mounted && (isBigScreen ? <Desktop links={links} /> : <Mobile links={links} />)}
+    </>
   );
 };
 
