94fdb784c0acc8d5e1ac2fa2bd4eae0d60e8aacc
Antonio Gatto <antonio.s.m.gatto@gmail.com>
2022-06-30 16:37:17 -0300

    fix warning "Do not add stylesheets using next/head "

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="./fonts/old-stamper.regular-webfont.css" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -41,8 +41,6 @@ export default function Home() {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
-
-        <link href="./fonts/old-stamper.regular-webfont.css" rel="stylesheet" />
       </Head>
 
       <Header />
