e5e131f39e6f7123d97762ded1fb299f2d7f9063
Antonio Gatto <antonio.s.m.gatto@gmail.com>
2022-06-30 15:20:52 -0300

    updating footer image and adding overlay

--- components/Footer.js
+++ components/Footer.js
@@ -13,7 +13,11 @@ const Footer = () => {
   return (
     <footer
       id="contact"
-      className="w-full h-screen bg-[url('/photos/diablos_mc_estrada_5.jpeg')] bg-bottom bg-cover bg-no-repeat flex flex-col justify-between text-center p-5 lg:px-20 lg:py-10"
+      className="w-full h-screen bg-bottom bg-cover bg-no-repeat flex flex-col justify-between text-center p-5 lg:px-20 lg:py-10"
+      style={{
+        backgroundImage:
+          "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5)), url('/photos/diablosmc-estrada-4.jpeg')",
+      }}
     >
       <div>
         <SectionTitle title="Sede" />
